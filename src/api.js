7bb1d148624e54330266f9f9f6e7ce62cc95c240
Abu Said Mohammad Readwanul <abusaidhridoy5@gmail.com>
2025-12-08 16:03:21 +0600

    fixed api

--- src/api.js
+++ src/api.js
@@ -2,5 +2,5 @@
 const envBase = import.meta.env.VITE_API_BASE;
 export const API_BASE =
   envBase ||
-  "https://artify-server-side-o1b3v4l9b-reawanuls-projects.vercel.app";
+  "https://artify-server-side-eosin.vercel.app/";
 
