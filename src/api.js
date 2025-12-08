a0a6143191f005d8f0729b426408a4da1e254ce2
Abu Said Mohammad Readwanul <abusaidhridoy5@gmail.com>
2025-12-08 17:20:24 +0600

    api vercel url change

--- src/api.js
+++ src/api.js
@@ -2,5 +2,5 @@
 const envBase = import.meta.env.VITE_API_BASE;
 export const API_BASE =
   envBase ||
-  "https://artify-server-side-3gs96icuu-reawanuls-projects.vercel.app";
+  "https://artify-server-side-eosin.vercel.app";
 
